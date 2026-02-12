2558c5200d79ae237ef35a75b8faa9e78b91f245
Odemichel <olivier.demichel@gmail.com>
2026-02-12 14:35:19 +0100

    Update blog article

--- src/data/blogImages.ts
+++ src/data/blogImages.ts
@@ -1,9 +1,15 @@
 import type { ImageMetadata } from 'astro';
 
-import aeroxVsZwift from '~/assets/images/logos/aerox-vs-zwift.png';
+import aeroxVsZwift from '~/assets/images/blog/aerox-vs-zwift.png';
+import PogacatVsEvenPoel from '~/assets/images/blog/Amstel-Pog_vs_EvenP.png';
+import GainChronoAeroEn from '~/assets/images/blog/gain-chrono-aero-en.png';
+import GainChronoAeroFr from '~/assets/images/blog/gain-chrono-aero-fr.png';
 // Ajoute ici toutes les autres images
 
 export const blogImages: Record<string, ImageMetadata> = {
   'aerox-vs-zwift': aeroxVsZwift,
+  'Pog_EvenP': PogacatVsEvenPoel,
+  "gain-chrono-aero-en": GainChronoAeroEn,
+  "gain-chrono-aero-fr": GainChronoAeroFr
   // etc.
 };
