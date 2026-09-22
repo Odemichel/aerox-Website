9a474ff47d35d323f6703abf30c2d525214afd06
Odemichel <olivier.demichel@gmail.com>
2026-09-22 16:20:47 +0200

    retrait offre Pionnier : pages supprimées, redirection 301 vers tarifs

--- src/config/links.ts
+++ src/config/links.ts
@@ -1,6 +1,5 @@
 // src/config/links.ts
 export const LINKS = {
-  download: "/telechargement/DeviensPionnier",
   abonnement: "/abonnement",
   pricing: "/#pricing",
   success: "/telechargement/success",
